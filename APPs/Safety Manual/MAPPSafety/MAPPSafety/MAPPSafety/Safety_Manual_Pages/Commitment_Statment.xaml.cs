1b1e1c9086658092e6a266fe3df0c04939b2f378
Clayton Samson <csamso1@lsu.edu>
2017-07-31 13:20:02 -0500

    Created MAPPSafety APP
    begun working on basic navigation

---
+++ APPs/Safety Manual/MAPPSafety/MAPPSafety/MAPPSafety/Safety_Manual_Pages/Commitment_Statment.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MAPPSafety.Safety_Manual_Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Commitment_Statment : ContentPage
+    {
+        public Commitment_Statment()
+        {
+            InitializeComponent();
+        }
+    }
+}
