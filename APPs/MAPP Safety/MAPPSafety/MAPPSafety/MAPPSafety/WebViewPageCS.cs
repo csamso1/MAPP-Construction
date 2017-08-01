cb4e4bc7b5c667cebfee66b86695dad84ebaceb3
Clayton Samson <csamso1@lsu.edu>
2017-08-01 14:25:20 -0500

    Created WebViewPageCS.cs
    also updated PDF file name to have no white space

---
+++ APPs/MAPP Safety/MAPPSafety/MAPPSafety/MAPPSafety/WebViewPageCS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MAPPSafety
+{
+    class WebViewPageCS : ContentPage
+    {
+        public WebViewPageCS()
+        {
+            Padding = new Thickness(0, 20, 0, 0);
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new CustomWebView
+                    {
+                        Uri = "FM-001_Weekly_Equipment_Inspection_Form.pdf",
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        VerticalOptions = LayoutOptions.FillAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
